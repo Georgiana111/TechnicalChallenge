935429c070891bec77a698e5c2228f64e792bcb1
96geo <96geo@DESKTOP-3JAC2VG>
2023-07-11 01:17:47 +0300

    Add project files.

---
+++ TechnicalChallenge/OTP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace TechnicalChallenge
+{
+    internal class OTP
+    {
+
+        public static string GeneratePassword(string userId, DateTime dateTime)
+        {
+            string inputString = userId + dateTime.ToString("yyyyMMddHHmmss");
+            byte[] inputBytes = Encoding.UTF8.GetBytes(inputString);
+
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                string password = Convert.ToBase64String(hashBytes);
+                return password;
+            }
+        }
+    }
+
+}
---
+++ TechnicalChallenge/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TechnicalChallenge
+{
+    internal class Program
+    {
+        private const int Seconds = 130;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Id:");
+            string userId = Console.ReadLine();
+            Console.WriteLine("Date and time (MM/dd/yyyy hh:mm:ss tt):");
+            string dateTime = Console.ReadLine();
+
+            DateTime date;
+
+            try
+            {
+                date = DateTime.Parse(dateTime);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid date/time format. Please try again.");
+                return;
+            }
+
+            string password = OTP.GeneratePassword(userId, date);
+            Console.WriteLine("Generated Password: " + password);
+
+            TimeSpan timeDifference = DateTime.Now - date;
+            Console.WriteLine("Time Difference (seconds): " + timeDifference.TotalSeconds);
+
+            if (timeDifference.TotalSeconds > Seconds)
+            {
+                Console.WriteLine("Password has expired. Generate a new password.");
+                password = OTP.GeneratePassword(userId, DateTime.Now);
+                Console.WriteLine("New Password: " + password);
+            }
+            else
+            {
+                Console.WriteLine("Password is still valid.");
+            }
+        }
+    }
+}
